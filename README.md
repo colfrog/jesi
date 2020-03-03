7fbb1a697a71176e47ee9bed8182b4a96c561459
Laurent Cimon <laurent@nilio.ca>
2020-03-03 00:20:18 -0500

    Remove bin/ from the repository

--- README.md
+++ README.md
@@ -7,13 +7,6 @@
 ```sh
 $ git clone https://github.com/colfrog/jesi.git
 $ cd jesi
-$ yarn install # or npm install
-$ yarn start # or npm start
-```
-
-### Contribute
-
-```
 $ yarn install # or npm install --production=false
 $ yarn build # or npm build
 $ yarn start # or npm start
--- bin/entry.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _irc = _interopRequireDefault(require("./irc/irc"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var client = new _irc["default"]();
-client.add_server('Snoonet', 'irc.snoonet.org', 6697, true);
