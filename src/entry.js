0f6b6ece4cb36ec34bb094d845123c2d80fd8b18
Laurent Cimon <laurent@nilio.ca>
2020-03-05 06:07:25 -0500

    Add multiple servers to insure consistent parsing

--- src/entry.js
+++ src/entry.js
@@ -2,16 +2,31 @@ import ServerInfo from './irc/ServerInfo';
 import UserInfo from './irc/UserInfo';
 import IRC from './irc/IRC';
 
-var servInfo = new ServerInfo({
-	name: 'Snoonet',
-	host: 'irc.snoonet.org',
-	port: 6697
-});
+var client = new IRC();
+// TODO: Parse userInfo and servInfo from a configuration file
 var userInfo = new UserInfo({
 	realname: 'Jessie Jane',
 	ident: 'jesi',
 	nick: 'jesi'
 });
+var servInfo = new ServerInfo({
+	name: 'Snoonet',
+	host: 'irc.snoonet.org',
+	port: 6697 // TLS is known from the port
+});
 
-var client = new IRC();
+client.addServer(servInfo, userInfo);
+
+servInfo = new ServerInfo({
+	name: 'Rizon',
+	host: 'irc.rizon.net',
+	port: 6697
+});
+client.addServer(servInfo, userInfo);
+
+servInfo = new ServerInfo({
+	name: 'Freenode',
+	host: 'irc.freenode.net',
+	port: 6697
+});
 client.addServer(servInfo, userInfo);
