4c30d6c63f20a68645030289cf73ddbcd644503b
Laurent Cimon <laurent@nilio.ca>
2020-04-02 17:11:28 +0000

    I'm not changing this format ever again.

--- modules/intensify.js
+++ modules/intensify.js
@@ -2,7 +2,7 @@ function doIntensify(msgData) {
 	let text = msgData.tail;
 	let match = text.match(/^\[(.+)\]$/);
 	if (match) {
-		let response = '\x02\x0304[' + match[1].toUpperCase() + '\x0f\x02\x0304 INTENSIFIES]';
+		let response = '\x02\x0304[\x1d' + match[1].toUpperCase() + '\x0f\x02\x0304 \x1dINTENSIFIES\x1d]';
 		ircWriter.sendMessage(msgData.replyTarget, response);
 	}
 }
