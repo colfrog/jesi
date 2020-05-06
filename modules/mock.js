8cc256bbf320ae855911eed3a4fc965a4c5200d5
Michael Eisemann <japanese_kitkat@yahoo.co.jp>
2020-05-06 20:17:07 +0000

    Fixed and added Idea

--- modules/mock.js
+++ modules/mock.js
@@ -3,24 +3,43 @@ var jModule = {
   description: "Mocks the given message",
   permissions: {
     hasServerInfo: true,
-    hasIRCWriter: true
+    hasIRCWriter: true,
+    hasLogAccess: true
   }
 };
 
-function doMock(msgData) {
-  // gEt rId oF cOmMaNd
-  let text = msgData.tail.split(" ").slice(1).join(" ");
+function mockMessage(message) {
   let response = "";
-
-  for (let i = 0; i < text.length; i++) {
+  for (let i = 0; i < message.length; i++) {
     if (i % 2 == 0) {
-      response += text[i].toUpperCase();
+      response += message[i].toUpperCase();
     } else {
-      response += text[i];
+      response += message[i].toLowerCase();
     }
   }
-
-  ircWriter.sendMessage(msgData.replyTarget, response);
+  return response;
 }
 
+function doMock(msgData) {
+  // scan through last 30 messages of current channel
+  getLogs(msgData.replyTarget, 30, (err, rows) => {
+    if (err) return;
+    let message = msgData.tail
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    // only scan thru history if we have only 1 word on the line
+    if (msgData.tailWords.length == 2) {
+      for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        // check if it's the nick
+        if (row.nick == msgData.tailWords[1]) {
+          message = row.message;
+          break;
+        }
+      }
+    }
+    ircWriter.sendMessage(msgData.replyTarget, mockMessage(message));
+  });
+}
 addCommand("mock", "doMock");
