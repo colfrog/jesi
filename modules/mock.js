17ff9de9f045c2cb6963e514349df7e7611b7a6b
Laurent Cimon <laurent@nilio.ca>
2020-05-09 14:40:00 +0000

    Make the letters random, add target prefix

--- modules/mock.js
+++ modules/mock.js
@@ -11,7 +11,7 @@ var jModule = {
 function mockMessage(message) {
   let response = "";
   for (let i = 0; i < message.length; i++) {
-    if (i % 2 == 0) {
+    if (Math.floor(Math.random()*2) == 0) {
       response += message[i].toUpperCase();
     } else {
       response += message[i].toLowerCase();
@@ -24,6 +24,7 @@ function doMock(msgData) {
   // scan through last 30 messages of current channel
   getLogs(msgData.replyTarget, 30, (err, rows) => {
     if (err) return;
+    let target = msgData.nick;
     let message = msgData.tail
       .split(" ")
       .slice(1)
@@ -31,6 +32,7 @@ function doMock(msgData) {
 
     // Send the previous message when there is only %mock
     if (msgData.tailWords.length == 1) {
+      target = rows[0].nick;
       message = rows[0].message;
     // only scan thru history if we have only 1 word on the line
     } else if (msgData.tailWords.length == 2) {
@@ -38,13 +40,14 @@ function doMock(msgData) {
         let row = rows[i];
         // check if it's the nick
         if (row.nick == msgData.tailWords[1]) {
+          target = row.nick;
           message = row.message;
           break;
         }
       }
     }
 
-    ircWriter.sendMessage(msgData.replyTarget, mockMessage(message));
+    ircWriter.sendMessage(msgData.replyTarget, '<\x02' + target + '\x02> ' + mockMessage(message));
   });
 }
 
