ca99169e12889c716317e29220fb51b5e2bf8b5e
Laurent Cimon <laurent@nilio.ca>
2020-05-09 15:00:55 +0000

    full pingpong

--- modules/pingpong.js
+++ modules/pingpong.js
@@ -11,4 +11,9 @@ function pong(msgData) {
 	ircWriter.sendMessage(msgData.replyTarget, commandPrefix + 'pong');
 }
 
+function ping(msgData) {
+	ircWriter.sendMessage(msgData.replyTarget, commandPrefix + 'ping');
+}
+
 addCommand('ping', 'pong');
+addCommand('pong', 'ping');
