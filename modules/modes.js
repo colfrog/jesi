17dd3a3926716ceaedb25fb2bd93fbaeb5ea20e2
Laurent Cimon <laurent@nilio.ca>
2020-06-29 23:06:51 +0000

    Allow users to call %modes on someone else

--- modules/modes.js
+++ modules/modes.js
@@ -8,9 +8,20 @@ var jModule = {
 };
 
 function modes(msgData) {
-	let nick = msgData.nick;
+	var nick;
+	if (msgData.tailWords.length == 2)
+		nick = msgData.tailWords[1];
+	else
+		nick = msgData.nick;
+
+	let user = serverInfo.users[nick];
+	if (!user) {
+		console.log('nick not found: ' + nick);
+		console.log(user)
+	}
+
 	let chan = msgData.params[0];
-	ircWriter.sendMessage(msgData.replyTarget, serverInfo.users[nick].channels[chan]);
+	ircWriter.sendMessage(msgData.replyTarget, user.channels[chan]);
 }
 
 addCommand('modes', 'modes');
